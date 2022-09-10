1b7931c17b817fb0f8ad9d5353a90701a9bcae7d
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-09-10 14:00:26 -0300

    org

--- pds1/tps/aula0206.c
+++ pds1/tps/aula0206.c
@@ -12,3 +12,16 @@ int main()
 
     printf("%d", y);
 } // end main()
+/*
+Deve-se escrever um programa que leia uma letra e a mapeia para as letras no telefone fixo, para digitar uma senha bancária.
+O programa deve ler uma letra e mapear para um número conforme o descrito abaixo:
+a, b, c -> 2
+d, e, f -> 3
+g, h, i -> 4
+j, k, l -> 5
+m, n, o -> 6
+p, q, r -> 7
+s, t, u -> 8
+v, w, x -> 9
+y, z -> 0
+*/
