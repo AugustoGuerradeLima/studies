a6cbd54da84dabf1a633c01740c6ece57a9be990
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-09-16 00:23:40 -0300

    l3

--- pds1/listas/l3.c
+++ pds1/listas/l3.c
@@ -1,79 +1,79 @@
 #include <stdio.h>
-
-//----------------------------------------questao-1
+//---------------------------------------------------------------------------q1
 float max(float a, float b, float c)
 {
-	if(a>b && a>c){return(a);}
-	else
-	{
-		if(b>c){return(b);}
-		else{return (c);}
-	}//end if()
-}//end max()
-
-
+    if (a > b && a > c)
+    {
+        return (a);
+    }
+    else
+    {
+        if (b > c)
+        {
+            return (b);
+        }
+        else
+        {
+            return (c);
+        }
+    }
+} // end max()
+float min(float a, float b, float c)
+{
+    if (a < b && a < c)
+    {
+        return (a);
+    }
+    else
+    {
+        if (b < c)
+        {
+            return (b);
+        }
+        else
+        {
+            return (c);
+        }
+    }
+} // end max()
 void f1(void)
 {
-	float a,b,c;
-	float nmax;
-	printf("\nescolha 3 numeros:\n");
-	scanf("%f",&a);
-	scanf("%f",&b);
-	scanf("%f",&c);
-	
-	//funcao
-	nmax = max(a,b,c);
-
-	printf("\no maior numero e  %f",nmax);
-}//end f1()
-
-//--------------------------------------------questao2
-int parimpar(int n)
+    float a, b, c;
+    float nmax, nmin;
+    scanf("%f%f%f", &a, &b, &c);
+    nmax = max(a, b, c);
+    nmin = min(a, b, c);
+    printf("\n%f is the biggest number.\n", nmax);
+    printf("\n%f is the smallest number.\n", nmin);
+} // end f1()
+//---------------------------------------------------------------------------q2
+//---------------------------------------------------------------------------q3
+//-------------------------------------------------------------------------main
+int main(void)
 {
-	return(n%2);
-}//end parimpar()
-
-void f2(void)
-{
-	int n,simnao;
-	printf("\nescolha um numero\n");
-	scanf("%d",&n);
-	simnao = parimpar(n);
-	printf("\ne impar?: %d\n",simnao);
-}//end f2()
-
-int main()
-{	
-	printf("\nLISTA 3 AUGUSTO \n");
-
-	int c = 0;
-
-	do
-	{
-	printf("escolha uma opcao:\n");
-	printf("0-parar\n1-max\n2-par ou impar\n");
-	scanf("%d",&c);
-
-		switch(c)
-		{	
-			case 0:
-			break;
-			
-			case 1:
-			f1();
-			break;
-
-			case 2:
-			f2();
-			break;
-			
-			default:
-			printf("\nvalor invalido\n");
-			break;
-		}	
-	}
-	while(c!=0);
-	
-	//return
-	return(0);
-}//end main()
+    int c = 0;
+    // menu
+    do
+    {
+        printf("\n0 - stop  1 - q1\n2 - q2    3 - q3\n");
+        scanf("%d", &c);
+        switch (c)
+        {
+        case 0:
+            break;
+        case 1:
+            f1();
+            break;
+        /*case 2:
+            f2();
+            break;
+        case 3:
+            f3();
+            break;*/
+        default:
+            printf("\nvalue not found :(\n");
+            break;
+        }
+    } while (c != 0);
+    return (0);
+} // end main()
