99c43387b10d1676e57e12f67d4befb8e05ca893
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-09-28 14:50:27 -0300

    contributors

--- pds1/listas/l4.c
+++ pds1/listas/l4.c
@@ -1,60 +1,165 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <math.h>
-
-int fatorial(int n)
+//--------------------------------------------questao1a
+int fat(int n)
 {
-	if(n==0)return(1);//sempre precisa de um caso base
-	else return(n*fatorial(n-1));//sao feitas chamadas recursivas no mesmo numero pedido
-	/*de 14 acima pode dar overflow
-	a partir de 34 o problema de overflow da o resultado 0 e isso e continuo*/
-}//end fatorial()
-
-int primo(int n, int d)//parametro d funciona como divisor
+	if(n==0)return(1);
+	else return(n*fat(n-1));
+}
+void f1()
 {
-	/*if(n%d==0 && d>1)return(0);
-	if(d==1)return(1);*/
-
-	//codigo analogo
-	//colocar o d==1 antes ja garante que d>1 logo nao tem que escrever isso e torna o codigo menor
-	if(d==1)return(1);
-	if(n%d==0)return(0);//caso base
-	return(primo(n,d-1));
-}//end primo()
-
-/*
-1 - sabemos que a raiz quadrada de n e sempre um numero menor que n
-2 - ao colocar valores arbitrarios em um intervalo entre 1 e n podemos verificar se os valores sao maiores ou menores que o resultado
-*/
-
-float raiz(float n, float chute)
+	int n = 0 ;
+	scanf("%d",&n);
+	fat(n);
+}
+//--------------------------------------------questao1b
+int mdc(int a, int b)
 {
-	if(chute*chute >= n-0.001 && chute*chute <= n+0.001)return(chute);/*ja retorna o chute se ele estiver correto 
-									e o intervalo define uma margem de erro ja que a precisao
-									absoluta nao e necessaria*/
-	if(chute*chute > n) return(raiz(n,chute*0.5));//se subtrair inteiro da ruim mas 0.001 nao tem memoria suficiente
-	if(chute*chute < n) return(raiz(n,chute*1.5));
-}//end raiz()
-
-int main()
+	if(a<b)return(mdc(b,a));
+	if(b==0)return(a);
+	return(mdc(b,a%b));
+}
+void f2()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("\no mdc e: %d\n",mdc(a,b));
+}
+//--------------------------------------------questao1c
+int mdc3(int a,int b,int c)
+{
+	return(mdc(a,mdc(b,c)));
+}
+void f3()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	printf("\no mdc e: %d\n",mdc3(a,b,c));
+}
+//--------------------------------------------questao1d
+int fib(int n)
+{
+	if(n<2)return(1);
+	return(fib(n-1)+fib(n-2));
+}
+void f4()
 {
 	int n = 0;
-	float f =0.0;
-	float chute = 0.0;
-
-	//fatorial
 	scanf("%d",&n);
-	printf("o fatorial de %d e %d\n",n,fatorial(n));
-
-	//primo
+	printf("%d",fib(n));
+}
+//--------------------------------------------questao1e
+int primo(int n, int d)
+{
+	if(d==1)return(1);
+	if(n%d==0)return(0);
+	return(primo(n,d-1));
+}
+void f5()
+{
+	int n = 0;
+	scanf ("%d",&n);
+	printf("%d",primo(n,sqrt(n)));
+}
+//--------------------------------------------questao1f
+void decrescente(int n)
+{
+	if(n==1){printf("%d\n",n);}
+	else
+	{
+	printf("%d\n",n);
+	decrescente(n-1);
+	}
+}
+void f6()
+{
+	int n;
 	scanf("%d",&n);
-	/*printf("o numero %d e primo: %d\n",n,primo(n,n-1));*/
-	//colocar raiz e uma funcao melhor
-	printf("o numero %d e primo: %d\n",n,primo(n,sqrt(n)));
-
-	//raiz
-	scanf("%f,%f",&f,&chute);
-	printf("raiz quadrada de %f e %f: \n",f,raiz(f,chute));
-
-	return(0);
-}//end main()
+	decrescente(n);
+}
+//--------------------------------------------questao1g
+int res(int a,int b)
+{
+	if(a-b<0)return(a);
+	return(res(a-b,b));
+}
+void f7()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("\nresto: %d\n",res(a,b));
+}
+//--------------------------------------------questao2(main)
+int main()
+{
+int c = 0;
+do
+{
+printf("\n0 - parar");
+printf("\n1 - fatorial");
+printf("\n2 - mdc");
+printf("\n3 - mdc3");
+printf("\n4 - fibonacci");
+printf("\n5 - primo");
+printf("\n6 - decrescente");
+printf("\n7 - resto");
+printf("\n8 - somatorio");
+printf("\n9 - mmc");
+printf("\n10 - divisao");
+printf("\n11 - raiz quadrada");
+printf("\n12 - soma digitos");
+printf("\n13 - exponecial");
+printf("\n14 - crescente\n");
+scanf("%d",&c);
+switch(c)
+{
+	case 0:
+	printf("\ntchauzinho :)\n");
+	break;
+	case 1:
+	f1();
+	break;
+	case 2:
+	f2();
+	break;
+	case 3:
+	f3();
+	break;
+	case 4:
+	f4();
+	break;
+	case 5:
+	f5();
+	break;
+	case 6:
+	f6();
+	break;
+	case 7:
+	f7();
+	break;
+	/*case 8:
+	f8();
+	break;
+	case 9:
+	f9();
+	break;
+	case 10:
+	f10();
+	break;
+	case 11:
+	f11();
+	break;
+	case 12:
+	f12();
+	break;
+	case 13:
+	f13();
+	break;*/
+	default:
+	printf("\nvalor invalido\n");
+	break;
+}
+}
+while(c!=0);
+return(0);
+}
---
+++ pds1/listas/recursivenotes.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int fatorial(int n)
+{
+	if(n==0)return(1);//sempre precisa de um caso base
+	else return(n*fatorial(n-1));//sao feitas chamadas recursivas no mesmo numero pedido
+	/*de 14 acima pode dar overflow
+	a partir de 34 o problema de overflow da o resultado 0 e isso e continuo*/
+}//end fatorial()
+
+int primo(int n, int d)//parametro d funciona como divisor
+{
+	/*if(n%d==0 && d>1)return(0);
+	if(d==1)return(1);*/
+
+	//codigo analogo
+	//colocar o d==1 antes ja garante que d>1 logo nao tem que escrever isso e torna o codigo menor
+	if(d==1)return(1);
+	if(n%d==0)return(0);//caso base
+	return(primo(n,d-1));
+}//end primo()
+
+/*
+1 - sabemos que a raiz quadrada de n e sempre um numero menor que n
+2 - ao colocar valores arbitrarios em um intervalo entre 1 e n podemos verificar se os valores sao maiores ou menores que o resultado
+*/
+
+float raiz(float n, float chute)
+{
+	if(chute*chute >= n-0.001 && chute*chute <= n+0.001)return(chute);/*ja retorna o chute se ele estiver correto 
+									e o intervalo define uma margem de erro ja que a precisao
+									absoluta nao e necessaria*/
+	if(chute*chute > n) return(raiz(n,chute*0.5));//se subtrair inteiro da ruim mas 0.001 nao tem memoria suficiente
+	if(chute*chute < n) return(raiz(n,chute*1.5));
+}//end raiz()
+
+int main()
+{
+	int n = 0;
+	float f =0.0;
+	float chute = 0.0;
+
+	//fatorial
+	scanf("%d",&n);
+	printf("o fatorial de %d e %d\n",n,fatorial(n));
+
+	//primo
+	scanf("%d",&n);
+	/*printf("o numero %d e primo: %d\n",n,primo(n,n-1));*/
+	//colocar raiz e uma funcao melhor
+	printf("o numero %d e primo: %d\n",n,primo(n,sqrt(n)));
+
+	//raiz
+	scanf("%f,%f",&f,&chute);
+	printf("raiz quadrada de %f e %f: \n",f,raiz(f,chute));
+
+	return(0);
+}//end main()
