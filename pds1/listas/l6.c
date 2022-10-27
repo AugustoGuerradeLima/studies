d78345ea705fd15152e9bf0a58df5a020146f158
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-10-27 13:36:17 -0300

    Update l6.c

--- pds1/listas/l6.c
+++ pds1/listas/l6.c
@@ -7,6 +7,12 @@ int prime(int n,int d){
 	if(n%d==0)return(0);
 	return(prime(n,d-1));
 }
+double function1(double x){
+    return((x*x) - (5.0*x) + 1.0);
+}
+double function2(double x, double a, double b, double c , double d){
+    return(a*x*x*x + b*x*x + c*x + d);
+}
 //---------------------------------------------------------------------------questao1
 void f1(){
     FILE*primes = fopen("PRIMES.txt","wt");
@@ -28,7 +34,7 @@ void f2(){
     FILE*file2 = fopen("YN.txt","wt");
     int n;
     while(!feof(file1)){
-        fscanf(file1,"%d",n);
+        fscanf(file1,"%d",&n);
         if(prime(n,sqrt(n))){
             fprintf(file2,"yes\n");
         }
@@ -38,16 +44,62 @@ void f2(){
     }
     fclose(file1);
     fclose(file2);
-} 
+}
+//---------------------------------------------------------------------------questao3
+void f3(){
+    FILE*file1 = fopen("NATURAL.txt","rt");
+    FILE*primos = fopen("PRIMOS.txt","wt");
+    FILE*outros = fopen ("OUTROS.txt","wt");
+    int n;
+    while(!feof(file1)){
+        fscanf(file1,"%d",&n);
+        if(prime(n,sqrt(n))){
+            fprintf(primos,"%d\n",n);
+        }
+        else{
+            fprintf(outros,"%d\n",n);
+        }
+    }
+    fclose(file1);
+    fclose(primos);
+    fclose(outros);
+}
+//---------------------------------------------------------------------------questao4
+void f4(){
+    FILE*file1 = fopen("INPUT1.txt","rt");
+    FILE*file2 = fopen("OUTPUT1.txt","wt");
+    double n;
+    while(!feof(file1)){
+        fscanf(file1,"%lf",&n);
+        n = function1(n);
+        fprintf(file2,"%lf\n",n);
+    }
+    fclose(file1);
+    fclose(file2);
+}
+//---------------------------------------------------------------------------questao5
+void f5(){
+    FILE*file1 = fopen("INPUT1.txt","rt");
+    FILE*file2 = fopen("OUTPUT2.txt","wt");
+    double a,b,c,d,n;
+    scanf("%lf%lf%lf%lf",&a,&b,&c,&d);
+    while(!feof(file1)){
+        fscanf(file1,"%lf",&n);
+        n = function2(n,a,b,c,d);
+        fprintf(file2,"%lf\n",n);
+    }
+    fclose(file1);
+    fclose(file2);
+}
 //---------------------------------------------------------------------------main
 int main(){
     int c;
     do
     {
         printf("\n0-Stop");
-        printf("\n1-Write prime numbers");
-        printf("\n2-Read natural numbers");
-        printf("\n3-Read prime or not");
+        printf("\n1- prime list");
+        printf("\n2- prime or not 1");
+        printf("\n3- prime or not 2");
         printf("\n4-Write function results");
         printf("\n5-Create functions\n");
         scanf("%d",&c);
@@ -55,9 +107,9 @@ int main(){
             case 0:break;
             case 1: f1();break;
             case 2: f2();break;
-            /*case 3: f3();break;
+            case 3: f3();break;
             case 4: f4();break;
-            case 5: f5();break;*/
+            case 5: f5();break;
             default: printf("\ninvalid value\n");
         }
     } while (c!=0);
