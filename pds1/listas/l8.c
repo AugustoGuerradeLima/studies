a2adc437b59c402c4fad1dc98e1ffd4b3b93fcd5
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-05 21:16:32 -0300

    Update l8.c

--- pds1/listas/l8.c
+++ pds1/listas/l8.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
-void f1(){
-}
+//----------------------------------------------------------------------printmatrix
+void printMatrix(int m[][],int row, int column){
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<column; j++){
+            printf("%d\t"m[i][j]);
+        }
+        printf("\n");
+    }
+}//end printmatrix
 int main(){
-return(0);
+    int c = 0;
+    do
+    {
+        printf("\n0-stop\n");
+        printf("\n1-file and matrix\n");
+        printf("\n2-matrix average\n");
+        printf("\n3-identity matrix\n");
+        printf("\n4-transposed matrix\n");
+        printf("\n5-symmetric matrix\n");
+        printf("\n6-sum\n");
+        printf("\n7-product\n");
+        scanf("%d",&c);
+        switch(c){
+            case 0:break;
+            case 1:f1();break;
+            case 2:f2();break;
+            case 3:f3();break;
+            case 4:f4();break;
+            case 5:f5();break;
+            case 6:f6();break;
+            case 7:f7();break;
+            default:printf("\ninvalid value\n");
+        }
+    } while (c!=0);
+    return(0);
 }
