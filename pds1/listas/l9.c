b0f2bb5cd10a4899a37a7799c6dbf3f52a2ae926
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-11 13:26:56 -0300

    Update l9.c

--- pds1/listas/l9.c
+++ pds1/listas/l9.c
@@ -14,6 +14,8 @@ struct Triangulo {
 	Ponto p1,p2,p3;
 	float perimetro();
     float area();
+	bool equilatero();
+	bool semelhante(Triangulo& t);
 };
 typedef Triangulo ref_Triangulo;
 struct Retangulo{
@@ -28,42 +30,55 @@ struct Ciriculo {
 	float raio;
 	float perimetro();
     float area();
+	bool contem(Ponto& p);
+	bool contem(Triangulo& t);
+	bool contem(Retangulo& r);
+	bool pertence(Ponto& p);
+	bool circunscrita(Triangulo& t);
+	bool circunscrita(Retangulo& r);
 };
 typedef Ciriculo* ref_Ciriculo;
 //----------------------------------------------------------------------questao2
-float Ponto::distancia(Ponto& p)
-{
+float Ponto::distancia(Ponto& p){
 	return(sqrt((this->x - p.x)*(this->x - p.x) + (this->y - p.y)*(this->y - p.y)));
 }
 //----------------------------------------------------------------------questao3
-void Ponto::atribuir(float a, float b)
-{
+void Ponto::atribuir(float a, float b){
 	this->x=a;
 	this->y=b;
 }
 //----------------------------------------------------------------------questao5
-float Triangulo::perimetro()
-{
+float Triangulo::perimetro(){
 	return(this->p1.distancia(this->p2)+this->p2.distancia(this->p3)+this->p3.distancia(this->p1));
 }
 //----------------------------------------------------------------------questao6
-float Triangulo::area()
-{
+float Triangulo::area(){
     return(sqrt((this->perimetro()/2)*(this->perimetro()/2 - this->p1.distancia(this->p2))*(this->perimetro()/2 - this->p2.distancia(this->p3))*(this->perimetro()/2 - this->p3.distancia(p1))));
 }
-//q7
-//q8
+//----------------------------------------------------------------------questao7
+bool Triangulo::equilatero(){
+	if(this->p1.distancia(this->p2)==this->p2.distancia(this->p3)&&this->p1.distancia(this->p2)==this->p1.distancia(this->p3))return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao8
+bool Triangulo::semelhante(Triangulo& t){
+	float a = this->p1.distancia(this->p2)/t.p1.distancia(t.p2); 
+	float b = this->p2.distancia(this->p3)/t.p2.distancia(t.p3);
+	float c = this->p3.distancia(this->p1)/t.p3.distancia(t.p1);
+	if(a==b&&a==c)return(true);
+	return(false);
+}
 //----------------------------------------------------------------------questao10
 float Retangulo::perimetro(){
-    return(this->p1.distancia(p2)+this->p2.distancia(p3)+this->p3.distancia(p4)+this->p4.distancia(p1));
+    return(this->p1.distancia(this->p2)+this->p2.distancia(this->p3)+this->p3.distancia(this->p4)+this->p4.distancia(this->p1));
 }
 //----------------------------------------------------------------------questao11
 float Retangulo::area(){
-    return(this->p1.distancia(p2)*this->p2.distancia(p3));
+    return(this->p1.distancia(this->p2)*this->p2.distancia(this->p3));
 }
 //----------------------------------------------------------------------questao12
 bool Retangulo::quadrado(){
-    if(this->p1.distancia(p2)==this->p2.distancia(p3)&&this->p3.distancia(p4)==this->p4.distancia(p1))return(true);
+    if(this->p1.distancia(this->p2)==this->p2.distancia(this->p3)&&this->p3.distancia(this->p4)==this->p4.distancia(this->p1))return(true);
     return(false);
 }
 //----------------------------------------------------------------------questao14
@@ -74,14 +89,38 @@ float Ciriculo::perimetro(){
 float Ciriculo::area(){
     return(M_PI*this->raio*this->raio);
 }
-//q16
-//q17
-//q18
-//q19
-//q20
-//q21
+//----------------------------------------------------------------------questao16
+bool Ciriculo::contem(Ponto& p){
+	if(this->raio >= p.distancia(this->centro))return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao17
+bool Ciriculo::contem(Triangulo& t){
+	if(this->raio>=t.p1.distancia(this->centro)&&this->raio>=t.p2.distancia(this->centro)&&this->raio>=t.p3.distancia(this->centro))return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao18
+bool Ciriculo::contem(Retangulo& r){
+	if(this->raio>=r.p1.distancia(this->centro)&&this->raio>=r.p2.distancia(this->centro)&&this->raio>=r.p3.distancia(this->centro)&&this->raio>=r.p4.distancia(this->centro))return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao19
+bool Ciriculo::pertence(Ponto& p){
+	if(this->raio == p.distancia(this->centro))return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao20
+bool Ciriculo::circunscrita(Triangulo& t){
+	if(t.p1.distancia(this->centro)==this->raio&&t.p2.distancia(this->centro)==this->raio&&t.p3.distancia(this->centro)==this->raio)return(true);
+	return(false);
+}
+//----------------------------------------------------------------------questao21
+bool Ciriculo::circunscrita(Retangulo& r){
+	if(r.p1.distancia(this->centro)==this->raio&&r.p2.distancia(this->centro)==this->raio&&r.p3.distancia(this->centro)==this->raio&&r.p4.distancia(this->centro)==this->raio)return(true);
+	return(false);
+}
 //main
 int main()
 {
-
+return(0);
 }
