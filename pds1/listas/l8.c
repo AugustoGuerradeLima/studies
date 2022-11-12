cefde47575afc0a918ce592b0ea467af832e5507
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-12 14:53:51 -0300

    Update l8.c

--- pds1/listas/l8.c
+++ pds1/listas/l8.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-//----------------------------------------------------------------------printmatrix
+//----------------------------------------------------------------------questao1
+void readMatrix(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int matrix[r][c];
+    for(int i = 0; i<r; i++){
+        for(int j = 0; j<c;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+}//end readmatrix
 void printMatrix(int m[][],int row, int column){
     for(int i = 0; i<row; i++){
         for(int j = 0; j<column; j++){
@@ -8,36 +18,71 @@ void printMatrix(int m[][],int row, int column){
         printf("\n");
     }
 }//end printmatrix
-//----------------------------------------------------------------------questao1
 //----------------------------------------------------------------------questao2
-//----------------------------------------------------------------------questao3
-//----------------------------------------------------------------------questao4
-//----------------------------------------------------------------------questao5
-//----------------------------------------------------------------------questao6
-int main(){
-    int c = 0;
-    do
-    {
-        printf("\n0-stop\n");
-        printf("\n1-file and matrix\n");
-        printf("\n2-matrix average\n");
-        printf("\n3-identity matrix\n");
-        printf("\n4-transposed matrix\n");
-        printf("\n5-symmetric matrix\n");
-        printf("\n6-sum\n");
-        printf("\n7-product\n");
-        scanf("%d",&c);
-        switch(c){
-            case 0:break;
-            case 1:f1();break;
-            case 2:f2();break;
-            case 3:f3();break;
-            case 4:f4();break;
-            case 5:f5();break;
-            case 6:f6();break;
-            case 7:f7();break;
-            default:printf("\ninvalid value\n");
+float mediaMatrix(int n, float mat[][100]){
+    float soma = 0.0;
+    for(int i = 0; i<n ; i++){
+        for(int j = 0; j<n;j++){
+            soma += mat[i][j];
         }
-    } while (c!=0);
+    }
+    return(soma/(float)(n*n));
+}
+//----------------------------------------------------------------------questao3
+void identidade(int n, float A[][100]){
+    for(int i= 0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(j==i){
+                A[i][j]=1.0;
+            }
+            else{
+                A[i][j]=0.0;
+            }
+        }
+    }
+}
+//----------------------------------------------------------------------questao4
+void transposta(int n, float A[][100], float T[][100]){
+    for(int i = 0; i<n;i++){
+        for(int j = 0;j<n;j++){
+            T[i][j]=A[j][i];
+        }
+    }
+}
+//----------------------------------------------------------------------questao5
+bool simetrica(int n, float A[][100]){
+    float mat[n][n];
+    transposta(n, A ,mat);
+    for(int i = 0;i<n;i++){
+        for(int j = 0; j<n;j++){
+            if(A[i][j]==mat[i][j]){
+                //ok
+            }
+            else{return(false);}
+        }
+    }
+    return(true);
+}
+//----------------------------------------------------------------------questao6
+void somaMatrix(int n, float A[][100],float B[][100], float S[][100]){
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            S[i][j]==A[i][j]+B[i][j];
+        }
+    }
+}
+//----------------------------------------------------------------------questao7
+void multMatrix(int n, float A[][100], float B[][100], float P[][100]){
+    for(int i=0; i<n;i++){
+        for(int j = 0; j<n;j++){
+            for(int k = 0;k<n;k++){
+                P[i][j] += A[i][k]*B[k][j];
+            }
+        }
+    }
+}
+//----------------------------------------------------------------------main
+int main(){
+
     return(0);
 }
