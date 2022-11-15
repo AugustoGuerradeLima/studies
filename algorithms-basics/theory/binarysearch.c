8361d4655d010402abbe3e39cbc88c24324c449e
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-15 10:07:00 -0300

    Create binarysearch.c

---
+++ algorithms-basics/theory/binarysearch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int binarySearch(int* array, int guess, int value, int size){
+    int min = 0;
+    int max = size - 1;
+    while(min<=max){
+        if(*(array + guess)==value){
+            return(guess);
+        }
+        if(*(array + guess)<value){
+            max = guess - 1;
+        }
+        if(*(array + guess)>value){
+            min = guess + 1;
+        }
+    }
+    return(-1);
+}
+int main(){
+    return(0);
+}
