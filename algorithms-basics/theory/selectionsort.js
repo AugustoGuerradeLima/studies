4610edd9e96456fa031d7a08434b00318afefde5
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-18 21:46:21 -0300

    Update selectionsort.js

--- algorithms-basics/theory/selectionsort.js
+++ algorithms-basics/theory/selectionsort.js
@@ -1,7 +1,7 @@
 var swap = function(array, firstIndex, secondIndex) {
-    var temp = array[firstIndex];
-    array[firstIndex] = array[secondIndex];
-    array[secondIndex] = temp;
+    var temp = array[firstIndex];//1
+    array[firstIndex] = array[secondIndex];//1
+    array[secondIndex] = temp;//1
 };
 
 var indexOfMinimum = function(array, startIndex) {
@@ -9,7 +9,7 @@ var indexOfMinimum = function(array, startIndex) {
     var minValue = array[startIndex];
     var minIndex = startIndex;
 
-    for(var i = minIndex + 1; i < array.length; i++) {
+    for(var i = minIndex + 1; i < array.length; i++) {//n^2/2+n/2
         if(array[i] < minValue) {
             minIndex = i;
             minValue = array[i];
@@ -18,7 +18,7 @@ var indexOfMinimum = function(array, startIndex) {
     return minIndex;
 }; 
 
-var selectionSort = function(array) {
+var selectionSort = function(array) {//Theta(n^2)
     var position;
     for(var i = 0; i<array.length;i++){
         position = indexOfMinimum(array,i);
