2ce732527eeb45941330d18a0fe90da27fc7691e
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-11-18 21:36:39 -0300

    Create selectionsort.js

---
+++ algorithms-basics/theory/selectionsort.js
@@ -0,0 +1,27 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var position;
+    for(var i = 0; i<array.length;i++){
+        position = indexOfMinimum(array,i);
+        swap(array,i,position);
+    }
+};
