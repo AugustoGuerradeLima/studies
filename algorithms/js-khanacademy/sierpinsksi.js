517965f65508627e481279d2b4f5d5232a3503c7
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-12-15 14:49:39 -0300

    algorithms

---
+++ algorithms/js-khanacademy/sierpinsksi.js
@@ -0,0 +1,22 @@
+var dim = 240;
+var minSize = 8;
+var drawGasket = function(x, y, dim) {
+    if (dim <= minSize) {
+	    rect(x, y, dim, dim);
+    }
+    else {
+	    var newDim = dim / 2;
+	    drawGasket(x, y, newDim);
+	    drawGasket(x + newDim, y, newDim);
+	    // drawGasket(x, y + newDim, newDim);
+	    drawGasket(x + newDim, y + newDim, newDim);
+    }
+};
+draw = function() {
+    background(255, 255, 255);
+    fill(255, 255, 0);
+    rect(0, 0, dim, dim);
+    stroke(0, 0, 255);
+    fill(0, 0, 255);
+    drawGasket(0, 0, dim);
+};
