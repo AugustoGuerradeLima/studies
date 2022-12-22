b175a1500f221a3ca2747f5b838d2863503edf23
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2022-12-22 14:09:43 -0300

    Update mergesort.js

--- algorithms/js-khanacademy/mergesort.js
+++ algorithms/js-khanacademy/mergesort.js
@@ -1,18 +1,8 @@
-// Takes in an array that has two sorted subarrays,
-//  from [p..q] and [q+1..r], and merges the array
-var merge = function(array, p, q, r) {
-    // This code has been purposefully obfuscated,
-    //  as you'll write it yourself in next challenge.
-    var a=[],b=[],c=p,d,e;for(d=0;c<=q;d++,c++){a[d]=array[c];}for(e=0;c<=r;e++,c++){b[e]=array[c];}c=p;for(e=d=0;d<a.length&&e<b.length;){if(a[d]<b[e]){array[c]=a[d];d++;} else {array[c]=b[e]; e++;}c++; }for(;d<a.length;){array[c]=a[d];d++;c++;}for(;e<b.length;){array[c]=b[e];e++;c++;}
-};
-
-
-// Takes in an array and recursively merge sorts it
-var mergeSort = function(array, p, r) {
-    
-};
-
-var array = [14, 7, 3, 12, 9, 11, 6, 2];
-mergeSort(array, 0, array.length-1);
-println("Array after sorting: " + array);
-//Program.assertEqual(array, [2, 3, 6, 7, 9, 11, 12, 14]);
+var mergeSort = function(array,p,r){
+    if(p<r){
+        var q = Math.floor((p+r)/2);
+        mergeSort(array,p,q);
+        mergeSort(array,q+1,r);
+        merge(array,p,q,r);
+    }
+}
