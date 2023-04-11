41d43b82c729b9b1bd7117397073f3acf71374e0
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-04-11 16:31:08 -0300

    Update Fatura.cpp

--- college/pds2/vpls/vpl4/Fatura.cpp
+++ college/pds2/vpls/vpl4/Fatura.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "Fatura.hpp"
 using namespace std;
-fatura::fatura(int id , double valor) : _id(id),_valor(valor){}
-void fatura::imprimir_dados(){cout<<_id<<" "<<_valor<<endl;}
-double fatura::getValor(){return _valor;}
+Fatura::Fatura(int id , double valor) : _id(id),_valor(valor){}
+void Fatura::imprimir_dados(){cout<<_id<<" "<<_valor<<endl;}
+double Fatura::getValor(){return _valor;}
