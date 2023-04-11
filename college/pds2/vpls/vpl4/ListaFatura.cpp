4b67b92d8a0b7c5ba105e7c9dcafd28eecafe7e3
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-04-11 16:32:08 -0300

    Update ListaFatura.cpp

--- college/pds2/vpls/vpl4/ListaFatura.cpp
+++ college/pds2/vpls/vpl4/ListaFatura.cpp
@@ -2,44 +2,44 @@
 #include "ListaFatura.hpp"
 #include "Fatura.hpp"
 using namespace std;
-celula::celula(ref_fatura dado):_dado(dado),_proximo(nullptr),_anterior(nullptr){};
-lista_fatura::lista_fatura():_inicio(nullptr),_fim(nullptr){};
-void lista_fatura::insere_fatura(ref_fatura dado){
-    ref_celula nova_celula = new celula(dado);//construtor da nova celula
+Celula::Celula(ref_Fatura dado):_dado(dado),_proximo(nullptr),_anterior(nullptr){};
+ListaFatura::ListaFatura():_inicio(nullptr),_fim(nullptr){};
+void ListaFatura::insere_Fatura(ref_Fatura dado){
+    ref_Celula nova_Celula = new Celula(dado);//construtor da nova Celula
     if(_inicio==nullptr){//verifica se a lista esta vazia
-        _inicio = nova_celula; 
-        _fim = nova_celula;
+        _inicio = nova_Celula; 
+        _fim = nova_Celula;
     }
-    else if(nova_celula->_dado->getValor() > _inicio->_dado->getValor()){//verifica se a celula sera o inicio
-        nova_celula->_proximo = _inicio;
-        _inicio->_anterior = nova_celula;
-        _inicio = nova_celula;
+    else if(nova_Celula->_dado->getValor() > _inicio->_dado->getValor()){//verifica se a Celula sera o inicio
+        nova_Celula->_proximo = _inicio;
+        _inicio->_anterior = nova_Celula;
+        _inicio = nova_Celula;
     }
     else{
-        ref_celula i = _inicio;//cria uma celula para iterar pela lista
-        while(nova_celula->_dado->getValor() < i->_dado->getValor()){
+        ref_Celula i = _inicio;//cria uma Celula para iterar pela lista
+        while(nova_Celula->_dado->getValor() < i->_dado->getValor()){
             i = i->_proximo;
             if(i == nullptr){//verifica se chegou no final da lista
-                _fim->_proximo = nova_celula;
-                nova_celula->_anterior = _fim;
-                _fim = nova_celula;
+                _fim->_proximo = nova_Celula;
+                nova_Celula->_anterior = _fim;
+                _fim = nova_Celula;
                 return;//sai da funcao para nao executar o codigo apos a iteracao
             }
         }
-        nova_celula->_anterior = i->_anterior;//insere a nova fatura na posicao
-        nova_celula->_proximo = i;
-        i->_anterior = nova_celula;
+        nova_Celula->_anterior = i->_anterior;//insere a nova Fatura na posicao
+        nova_Celula->_proximo = i;
+        i->_anterior = nova_Celula;
     }
 }
-ref_fatura lista_fatura::proxima_fatura(){
+ref_Fatura ListaFatura::proxima_Fatura(){
     if(_inicio==nullptr)return nullptr;
-    ref_celula resultado = _inicio;
+    ref_Celula resultado = _inicio;
     _inicio->_proximo->_anterior = nullptr;
     _inicio = _inicio->_proximo;
     return resultado->_dado;
 }
-void lista_fatura::imprimir_lista(){
-    ref_celula i = _inicio;
+void ListaFatura::imprimir_lista(){
+    ref_Celula i = _inicio;
     while(i->_proximo!=nullptr){
         i->_dado->imprimir_dados();
         i = i->_proximo;
