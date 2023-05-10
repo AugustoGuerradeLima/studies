3d98fd68b1193e26a70c2c17d800dd627e334337
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-05-10 10:53:45 -0300

    Update vpl7.cpp

--- college/pds2/vpls/vpl7.cpp
+++ college/pds2/vpls/vpl7.cpp
@@ -4,19 +4,17 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-int main(){
-    int n_testes;
-    while (true){
-        cin >> n_testes;
-        if (n_testes == 0)break;
-        map<string, int> especies;
-        for (int i = 0; i < n_testes; i++){
-            string mDNA;
-            cin >> mDNA;
-            sort(mDNA.begin(), mDNA.end());
-            especies[mDNA]++;
-        }
-        cout << especies.size() << endl;
+int main() {
+  int n_testes;
+  while (cin >> n_testes && n_testes != 0) {
+    map<string, int> especies;
+    for (int i = 0; i < n_testes; i++) {
+      string mDNA;
+      cin >> mDNA;
+      sort(mDNA.begin(), mDNA.end());
+      especies[mDNA]++;
     }
-    return (0);
+    cout << especies.size() << endl;
+  }
+  return 0;
 }
