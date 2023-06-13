5e26d4e36caadc2c50413dd6d43781ac398a9735
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-06-12 22:53:35 -0300

    Update main.cpp

--- pds2/vpls/vpl10/main.cpp
+++ pds2/vpls/vpl10/main.cpp
@@ -11,34 +11,46 @@ using std::ostream;
 using std::this_thread::sleep_for;
 using namespace std::chrono_literals;
 
-ostream & operator << (ostream &out, JogoDaVida &t) {
-  out << "  "; 
+// Sobrecarrega o operador << para imprimir o estado do JogoDaVida.
+ostream& operator<<(ostream& out, const JogoDaVida& t) {
+  out << "  ";
   for (int i = 0; i < t.colunas(); ++i) {
-    out << "X "; 
+    out << "X ";
   }
   out << endl;
   for (int i = 0; i < t.linhas(); ++i) {
     out << "X ";
     for (int j = 0; j < t.colunas(); ++j) {
       if (t.viva(i, j)) {
-        out << "o "; 
+        out << "o ";
       } else {
         out << "  ";
       }
     }
     out << "X" << endl;
   }
-  out << "  "; 
+  out << "  ";
   for (int i = 0; i < t.colunas(); ++i) {
-    out << "X "; 
+    out << "X ";
   }
   out << endl;
   return out;
 }
 
+ExcecaoCelulaInvalida::ExcecaoCelulaInvalida(int linha, int coluna)
+    : linha(linha), coluna(coluna) {
+  mensagemErro = "Célula (" + std::to_string(linha) + ", " +
+                 std::to_string(coluna) +
+                 ") não é válida. Deseja continuar e ignorá-la? (s/n)?\n";
+}
+
+const char* ExcecaoCelulaInvalida::what() const noexcept {
+  return mensagemErro.c_str();
+}
+
 int main() {
   int numero_de_iteracoes;
-  cin >> numero_de_iteracoes;    
+  cin >> numero_de_iteracoes;
 
   int numero_de_linhas;
   cin >> numero_de_linhas;
@@ -50,23 +62,21 @@ int main() {
   JogoDaVida jogo(numero_de_linhas, numero_de_colunas);
   int linha, coluna;
   while (cin >> linha >> coluna) {
-
-    try{
+    try {
       jogo.Reviver(linha, coluna);
-    } catch (ExcecaoCelulaInvalida &e){
+    } catch (ExcecaoCelulaInvalida& e) {
+      do {
+        cout << e.what();
+        cin >> continuar;
+      } while (continuar != 's' && continuar != 'n');
 
-      do{
-      cout << e.what();
-      cin >> continuar;
-
-      } while(continuar != 's' && continuar != 'n');
-
-      if(continuar == 'n')
+      if (continuar == 'n') {
         return 1;
+      }
     }
   }
   cout << jogo << endl;
-  
+
   for (int i = 0; i < numero_de_iteracoes; i++) {
     jogo.ExecutarProximaIteracao();
     cout << jogo << endl;
