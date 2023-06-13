d69a82139f5b25dfcf6d3425bb0972584a1e9a68
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-06-12 22:53:14 -0300

    Update jogo_da_vida.hpp

--- pds2/vpls/vpl10/jogo_da_vida.hpp
+++ pds2/vpls/vpl10/jogo_da_vida.hpp
@@ -1,38 +1,52 @@
-#include <exception>
-#include <string>
 #pragma once
 
+#include <exception>
+#include <string>
 #include <vector>
 
+// Implementa o Jogo da Vida de John Conway.
+// https://pt.wikipedia.org/wiki/Jogo_da_vida
 class JogoDaVida {
  public:
+  // Cria um jogo cujo torus tem l linhas e c colunas.
+  // Todas as células são inicializadas como mortas.
   JogoDaVida(int l, int c);
-  int linhas() {return vivas_.size();};
-  int colunas() {return vivas_[0].size();};
-  bool viva(int i, int j);
+
+  // Retorna o número de linhas do torus.
+  int linhas() const;
+
+  // Retorna o número de colunas do torus.
+  int colunas() const;
+
+  // Retorna o estado da célula [i, j].
+  bool viva(int i, int j) const;
+
+  // Altera o estado da célula [i, j].
   void Matar(int i, int j);
   void Reviver(int i, int j);
+
+  // Executa a próxima iteração do jogo da vida.
   void ExecutarProximaIteracao();
+
+  // Executa n iterações do jogo da vida.
   void Executar(int n);
+
  private:
-  int NumeroDeVizinhasVivas(int x, int y);
-  std::vector<std::vector<bool>> vivas_;  
+  // Conta o número de vizinhas vivas da célula [x, y].
+  int NumeroDeVizinhasVivas(int x, int y) const;
+
+  // Armazena o estado das células.
+  std::vector<std::vector<bool>> vivas_;
 };
 
 class ExcecaoCelulaInvalida : public std::exception {
-public:
-    ExcecaoCelulaInvalida(int linha, int coluna)
-        : linha(linha), coluna(coluna) {
-        mensagemErro = "Célula (" + std::to_string(linha) +
-                       ", " + std::to_string(coluna) + ") não é válida. Deseja continuar e ignorá-la? (s/n)?\n";
-    }
+ public:
+  ExcecaoCelulaInvalida(int linha, int coluna);
 
-    const char* what() const noexcept override {
-        return mensagemErro.c_str();
-    }
+  const char* what() const noexcept override;
 
-private:
-    int linha;
-    int coluna;
-    std::string mensagemErro;
+ private:
+  int linha;
+  int coluna;
+  std::string mensagemErro;
 };
