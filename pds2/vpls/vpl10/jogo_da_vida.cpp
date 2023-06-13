823c8f1b4c576745549c1dff5730e5d9c9b8dba3
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-06-12 22:54:03 -0300

    Update jogo_da_vida.cpp

--- pds2/vpls/vpl10/jogo_da_vida.cpp
+++ pds2/vpls/vpl10/jogo_da_vida.cpp
@@ -1,31 +1,39 @@
 #include "jogo_da_vida.hpp"
 
-JogoDaVida::JogoDaVida(int l, int c) 
+JogoDaVida::JogoDaVida(int l, int c)
     : vivas_(l, std::vector<bool>(c, false)) {
 }
 
-bool JogoDaVida::viva(int i, int j) {
+int JogoDaVida::linhas() const {
+  return vivas_.size();
+}
+
+int JogoDaVida::colunas() const {
+  return vivas_[0].size();
+}
+
+bool JogoDaVida::viva(int i, int j) const {
   return vivas_[i][j];
 }
 
 void JogoDaVida::Matar(int i, int j) {
   vivas_[i][j] = false;
 }
-void JogoDaVida::Reviver(int i, int j) {
 
-  if(i >= this->linhas() || i < 0 || j >= this->colunas() || j < 0){
-    throw *new ExcecaoCelulaInvalida(i, j);
-  } else{
+void JogoDaVida::Reviver(int i, int j) {
+  if (i >= 0 && i < linhas() && j >= 0 && j < colunas()) {
     vivas_[i][j] = true;
+  } else {
+    throw ExcecaoCelulaInvalida(i, j);
   }
 }
 
-int JogoDaVida::NumeroDeVizinhasVivas(int x, int y) {
+int JogoDaVida::NumeroDeVizinhasVivas(int x, int y) const {
   int vizinhas_vivas = 0;
   for (int i : {x - 1, x, x + 1}) {
     for (int j : {y - 1, y, y + 1}) {
-      if (i != x || j != y) {
-        int i_circular = (i + linhas()) % linhas();   
+      if (i != x || j != y) {  // Não conta a própria célula [x, y].
+        int i_circular = (i + linhas()) % linhas();
         int j_circular = (j + colunas()) % colunas();
         if (viva(i_circular, j_circular)) {
           vizinhas_vivas++;
@@ -37,15 +45,15 @@ int JogoDaVida::NumeroDeVizinhasVivas(int x, int y) {
 }
 
 void JogoDaVida::ExecutarProximaIteracao() {
-  JogoDaVida novo(linhas(), colunas());
+  JogoDaVida novo(linhas(), colunas());  // Cria um novo objeto, com todas as células mortas.
   for (int i = 0; i < linhas(); i++) {
     for (int j = 0; j < colunas(); j++) {
       int n = NumeroDeVizinhasVivas(i, j);
-       if(n == 3 || (n == 2 && viva(i, j))) {
-         novo.Reviver(i, j);
-       } 
+      if (n == 3 || (n == 2 && viva(i, j))) {
+        novo.Reviver(i, j);
+      }
     }
-  } 
+  }
   *this = novo;
 }
 
