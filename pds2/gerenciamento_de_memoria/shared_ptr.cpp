4bafd1bb487b7115491f2635d158f44b0764c18e
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-06-22 10:41:39 -0300

    remove gen_memory

--- pds2/gerenciamento_de_memoria/shared_ptr.cpp
+++
@@ -1,9 +0,0 @@
-{
-  auto s1 = std::make_shared<int>(42);
-
-  {
-    // Ok: a cópia significa compartilhamento do mesmo recurso. Contador incrementa.
-    std::shared_ptr<int> s2 = s1;
-  } // s2 sai de escopo e o contador decrementa.
-
-} // s1 sai de escopo, contador decrementa e descarta o recurso.
--- pds2/gerenciamento_de_memoria/unique_ptr_passar_posse.cpp
+++
@@ -1,6 +0,0 @@
-auto p1 = std::make_unique<int>(42); // `std::make_unique` é C++14.
-
-//std::unique_ptr<int> p2 = p1; // Errado: não é possível copiar um `std::unique_ptr`.
-
-std::unique_ptr<int> p3 = std::move(p1); // Ok: posse do recurso foi transferida.
--
