b54360141928892aa15da63ffb9af3417302467b
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2024-06-09 17:02:58 -0300

    update grafos

--- ED/grafos/lista.cc
+++ ED/grafos/lista.cc
@@ -4,20 +4,22 @@ using namespace std;
 class no {
 public:
     int item;
+    double peso;
     no* prox;
 
-    no(int _item) : item(_item), prox(nullptr) {}
+    no(int _item) : item(_item), peso(1.0),  prox(nullptr) {}
+
+    no(int _item, double _peso) : item(_item), peso(_peso), prox(nullptr) {}
 };
 
 class lista {
-private:
+public:
     no* cabeca;
     int tamanho;
-
-public:
     lista() : cabeca(nullptr), tamanho(0) {}
     ~lista();
-    void insert(int _item);
+    void insert(int _item, double _peso=1.0);
+    int dequeue();
     void print() const;
 };
 
@@ -31,21 +33,30 @@ lista::~lista()
     }
 }
 
-void lista::insert(int _item) 
+void lista::insert(int _item, double _peso) 
 {
-    no* novo = new no(_item);
+    no* novo = new no(_item, _peso);
     novo->prox = this->cabeca;
     cabeca = novo;
     tamanho++;
+}
+
+int lista::dequeue()
+{
+	no* p = this->cabeca;
+	int _item = p->item;
+	this->cabeca = this->cabeca->prox;
+	delete p;
+	tamanho--;
+	return(_item);
 }
 
 void lista::print() const 
 {
     no* atual = this->cabeca;
     while (atual != nullptr) {
-        cout << atual->item << " -> ";
+        cout <<"("<< atual->item<<", "<< atual->peso << ") -> ";
         atual = atual->prox;
     }
     cout << "NULL" << endl;
 }
-
